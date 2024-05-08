076228acef9ffb57ca80ea9524c43566cb1243f0
JakeBrxwn <135643418+JakeBrxwn@users.noreply.github.com>
2024-05-08 15:25:44 -0500

    Hi

---
+++ scripts/demo_cam.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import rospy, cv2, cv_bridge, numpy
+
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+
+vc.release()
+cv2.destroyWindow("preview")
