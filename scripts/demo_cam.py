07e8977bf6480de294fd17dbbeb436fb6e85acee
JakeBrxwn <135643418+JakeBrxwn@users.noreply.github.com>
2024-05-09 15:28:56 -0500

    Test code for demo_cam

--- scripts/demo_cam.py
+++ scripts/demo_cam.py
@@ -1,21 +1,42 @@
 #!/usr/bin/env python3
 
 import rospy, cv2, cv_bridge, numpy
+from final_project.msg import GuessedLetter
+from std_msgs.msg import String
 
-cv2.namedWindow("preview")
-vc = cv2.VideoCapture(0)
+class demo_cam:
+    def __init__(self):
+        rospy.init_node('letter_finder')
+        self.grab_status = rospy.Subscriber('next_step', String, self.action_check)
+        self.letter_pub = rospy.Publisher('cur_letter', GuessedLetter, queue_size=1)
+        cv2.namedWindow('feed', 1)
+        self.latest_window = None
+        self.new_image = False
+        self.vc = cv2.VideoCapture(0)
 
-if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
-else:
-    rval = False
+    def camera_feed(self, msg):
+        if msg is not None:
+            if self.vc.isOpened(): # try to get the first frame
+                self.new_image = True
+                self.rval, self.frame = self.vc.read()
+            else:
+                self.rval = False
+        else:
+            while self.check == None:
+                rospy.sleep(0.5)
+    
+    def run(self):
+        rate = rospy.Rate(30)
+        while not rospy.is_shutdown():
+            if self.new_image:
+                cv2.imshow("feed", self.frame)
+                self.rval, self.frame = self.vc.read()
+                cv2.waitKey(20)
+                self.new_image = False
+            rate.sleep()
+        self.vc.release()
+        cv2.destroyWindow("feed")
 
-while rval:
-    cv2.imshow("preview", frame)
-    rval, frame = vc.read()
-    key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
-        break
-
-vc.release()
-cv2.destroyWindow("preview")
+if __name__ == '__main__':
+    demo = demo_cam()
+    demo.run()
