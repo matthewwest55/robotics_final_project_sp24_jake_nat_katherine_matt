194325b37cf478c1dada527152574458737ab9ca
nugerhard <ngerhard@uchicago.edu>
2024-05-10 14:42:03 -0500

    most of the way

--- scripts/demo_cam.py
+++ scripts/demo_cam.py
@@ -2,41 +2,78 @@
 
 import rospy, cv2, cv_bridge, numpy
 from final_project.msg import GuessedLetter
-from std_msgs.msg import String
 
-class demo_cam:
+from PIL import Image
+from model import CNN
+import torch
+
+import torchvision
+from torchvision.transforms import v2
+ 
+
+class GuessCam(object):
     def __init__(self):
-        rospy.init_node('letter_finder')
-        self.grab_status = rospy.Subscriber('next_step', String, self.action_check)
-        self.letter_pub = rospy.Publisher('cur_letter', GuessedLetter, queue_size=1)
-        cv2.namedWindow('feed', 1)
-        self.latest_window = None
-        self.new_image = False
-        self.vc = cv2.VideoCapture(0)
+        rospy.init_node('guess_letter')
+        rospy.Subscriber('finished_action', GuessedLetter, self.action_check)
+        self.letter_pub = rospy.Publisher('guessed_letter', GuessedLetter, queue_size=1)
+        self.alphabet = ['Null','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        self.guesses = ['Null']
+        self.new_image = True
+        self.cap = cv2.VideoCapture(0)
+        rospy.sleep(5)
+        self.run()
 
-    def camera_feed(self, msg):
-        if msg is not None:
-            if self.vc.isOpened(): # try to get the first frame
-                self.new_image = True
-                self.rval, self.frame = self.vc.read()
-            else:
-                self.rval = False
-        else:
-            while self.check == None:
-                rospy.sleep(0.5)
+    def action_check(self, data):
+        self.new_image = True
+
     
     def run(self):
-        rate = rospy.Rate(30)
+
+        transform = v2.Compose([
+            v2.Grayscale(),
+            v2.Resize((28, 28)),
+            v2.Lambda(lambda x: v2.functional.invert(x)),
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize((0.1307,), (0.3081,))
+            ])
+        
+        model = torch.load("letter_model.pt")
+        model.eval()
+
         while not rospy.is_shutdown():
-            if self.new_image:
-                cv2.imshow("feed", self.frame)
-                self.rval, self.frame = self.vc.read()
-                cv2.waitKey(20)
-                self.new_image = False
-            rate.sleep()
-        self.vc.release()
-        cv2.destroyWindow("feed")
+
+            ret, frame = self.cap.read()
+            if not ret:
+                print("Error capturing frame")
+                break
+            # From opencv BGR to PIL
+            rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            pil_image = Image.fromarray(rgb_image)
+            transformed_image = transform(pil_image)
+            with torch.no_grad():
+                output = model(transformed_image)
+            letter = self.alphabet[output.item()]
+
+            # Display the camera feed
+            cv2.imshow("Camera Feed", frame)
+            cv2.waitKey(1)
+
+            lettermsg = GuessedLetter()
+            lettermsg.letter = letter
+            self.guess_pub.publish(lettermsg)
+
+            """
+            if not (letter in self.guesses) and self.new_image:
+                self.new_image = False 
+                self.guesses.append(letter)
+                lettermsg = GuessedLetter()
+                lettermsg.letter = letter
+                self.guess_pub.publish(lettermsg)
+            """
+            rospy.sleep(1)
+            
+            
 
 if __name__ == '__main__':
-    demo = demo_cam()
-    demo.run()
+    demo = GuessCam()
