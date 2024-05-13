2b68d3461e1a687b9fadc562bfc6d6945be5ed2a
Katherine Marie Waterman <kwaterman210@csil5-29.cs.uchicago.edu>
2024-05-13 17:20:20 -0500

    added galley drawing

--- scripts/move_arm.py
+++ scripts/move_arm.py
@@ -28,14 +28,56 @@ class MoveArm(object):
         # self.move_group_arm.go([0,0,0,0], wait=True)
         print("ready")
 
-    def draw_line(self):
+    def draw_galley(self, starting_index, matrix):
+        #starting_index is a tuple bottom left corner of the galley
+        #matrix is the matrix of robot arm positions
         # In this function, we are going to have to pay mind to
         # moving multiple joints at once
         # Example: If the arm is high and begins moving down,
         # it needs to extend a bit to keep touching the board
         
         # Maybe we can make the joints a function of one another?
-        
+
+        #this portion draws the galley base
+        x, y = starting_index
+        for cell in range(0, 10):
+            pose_position = matrix[x + cell][y]
+            self.move_group_arm.go(pose_position)
+
+        #reset the default position here
+        ################################
+
+        #this portion draws the galley beam
+        for cell in range(0, 23):
+            pose_position = matrix[x + 5][y + cell]
+            self.move_group_arm.go(pose_position)
+
+        #this portion draws the galley top beam
+        for cell in range(0, 11):
+            pose_position = matrix[x + cell + 5][y + 22]
+            self.move_group_arm.go(pose_position)
+
+        #this portion draws the hanging portion of the galley
+        for cell in range(0, 6):
+            pose_position = matrix[x + 15][y - cell + 22]
+            self.move_group_arm.go(pose_position)
+
+    def draw_head(self, matrix, starting_position):
+        pass
+
+    def draw_body(self, matrix, starting_position):
+        pass
+
+    def draw_left_arm(self, matrix, starting_position):
+        pass
+
+    def draw_right_arm(self, matrix, starting_position):
+        pass
+    
+    def draw_left_leg(self, matrix, starting_position):
+        pass
+
+    def draw_right_leg(self, matrix, starting_position):
         pass
 
     def reset_arm(self):
