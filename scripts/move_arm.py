90f209f8c6df429970304fb21cded16738703461
Katherine Marie Waterman <kwaterman210@csil5-20.cs.uchicago.edu>
2024-05-15 18:12:00 -0500

    added code for letters E F H I K L M N T U and code for drawing the gallows + hangman

--- scripts/move_arm.py
+++ scripts/move_arm.py
@@ -41,20 +41,19 @@ class MoveArm(object):
         #this portion draws the galley base
         x, y = starting_index
         for cell in range(0, 10):
-            pose_position = matrix[x + cell][y]
+            pose_position = matrix[x + 10 + cell][y + 27]
             self.move_group_arm.go(pose_position)
 
-        #reset the default position here
-        ################################
+        self.reset_arm()
 
         #this portion draws the galley beam
         for cell in range(0, 23):
-            pose_position = matrix[x + 5][y + cell]
+            pose_position = matrix[x + 15][y + 27 - cell]
             self.move_group_arm.go(pose_position)
 
         #this portion draws the galley top beam
         for cell in range(0, 11):
-            pose_position = matrix[x + cell + 5][y + 22]
+            pose_position = matrix[x + cell + 5][y + 5]
             self.move_group_arm.go(pose_position)
 
         #this portion draws the hanging portion of the galley
@@ -62,23 +61,234 @@ class MoveArm(object):
             pose_position = matrix[x + 15][y - cell + 22]
             self.move_group_arm.go(pose_position)
 
-    def draw_head(self, matrix, starting_position):
-        pass
+    #this portion of code is responsible for drawing the body components
+    def draw_head(self, matrix, starting_index):
+        x, y = starting_index
+        for cell in range(0, 16):
+            pass
 
-    def draw_body(self, matrix, starting_position):
-        pass
+    def draw_body(self, matrix, starting_index):
+        x, y = starting_index
+        for cell in range(0, 7):
+            pose_position = matrix[x + 27][y + 16 + cell]
+            self.move_group_arm.go(pose_position)
 
-    def draw_left_arm(self, matrix, starting_position):
-        pass
+    def draw_left_arm(self, matrix, starting_index):
+        x, y = starting_index
+        for cell in range(0, 3):
+            pose_position = matrix[x + 27 - cell][y + 19 - cell]
+            self.move_group_arm.go(pose_position)
 
-    def draw_right_arm(self, matrix, starting_position):
-        pass
+    def draw_right_arm(self, matrix, starting_index):
+        x, y = starting_index
+        for cell in range(0, 3):
+            pose_position = matrix[x + 27 + cell][y + 19 - cell]
+            self.move_group_arm.go(pose_position)
     
-    def draw_left_leg(self, matrix, starting_position):
-        pass
+    def draw_left_leg(self, matrix, starting_index):
+        x, y = starting_index
+        for cell in range(0, 3):
+            pose_position = matrix[x + 27 - cell][y + 23 + cell]
+            self.move_group_arm.go(pose_position)
 
-    def draw_right_leg(self, matrix, starting_position):
-        pass
+    def draw_right_leg(self, matrix, starting_index):
+        x, y = starting_index
+        for cell in range(0, 3):
+            pose_position = matrix[x + 27 + cell][y + 23 + cell]
+            self.move_group_arm.go(pose_position)
+
+    ##This portion is responsible for drawing all alphabet letters
+    #Letters Completed:
+
+    def draw_A(self, matrix, starting_index):
+
+    def draw_B(self, matrix, starting_index):
+
+    def draw_C(self, matrix, starting_index):
+
+    def draw_D(self, matrix, starting_index):
+
+    def draw_E(self, matrix, starting_index):
+        #top left corner
+        x, y = starting_index
+        for cell in range(0, 5):
+            pose_position = matrix[x][y + cell]
+            self.move_group_arm.go(pose_position)
+
+        #reset position
+        for cell in range(0, 4):
+            pose_position = matrix[x + cell][y]
+            self.move_group_arm.go(pose_position)
+
+        #reset position
+        for cell in range(0, 3):
+            pose_position = matrix[x + cell][y + 2]
+            self.move_group_arm.go(pose_position)
+
+        #reset position
+        for cell in range(0, 4):
+            pose_position = matrix[x + cell][y + 4]
+            self.move_group_arm.go(pose_position)
+
+
+    def draw_F(self, matrix, starting_index):
+        #top left corner
+        x, y = starting_index
+        for cell in range(0, 5):
+            pose_position = matrix[x][y + cell]
+            self.move_group_arm.go(pose_position)
+
+        #reset position
+        for cell in range(0, 4):
+            pose_position = matrix[x + cell][y]
+            self.move_group_arm.go(pose_position)
+
+        #reset position
+        for cell in range(0, 3):
+            pose_position = matrix[x + cell][y + 2]
+            self.move_group_arm.go(pose_position)
+
+    def draw_G(self, matrix, starting_index):
+
+    def draw_H(self, matrix, starting_index):
+        x, y = starting_index
+        for cell in range(0, 5):
+            pose_position = matrix[x][y + cell]
+            self.move_group_arm.go(pose_position)
+
+        #reset position
+        for cell in range(0, 4):
+            pose_position = matrix[x + cell][y + 3]
+            self.move_group_arm.go(pose_position)
+        
+        #reset position
+        for cell in range(0, 5):
+            pose_position = matrix[x + 3][y + cell]
+            self.move_group_arm.go(pose_position)
+
+
+    def draw_I(self, matrix, starting_index):
+        x, y = starting_index
+        for cell in range(0, 5):
+            pose_position = matrix[x][y + cell]
+            self.move_group_arm.go(pose_position)
+
+    def draw_J(self, matrix, starting_index):
+
+    def draw_K(self, matrix, starting_index):
+        #top left corner
+        x, y = starting_index
+        for cell in range(0, 5):
+            pose_position = matrix[x][y + cell]
+            self.move_group_arm.go(pose_position)
+
+        #reset position
+        for cell in range(0, 3):
+            pose_position = matrix[x + cell][y + 3 - cell]
+            self.move_group_arm.go(pose_position)
+
+        #reset position
+        for cell in range(0, 3):
+            pose_position = matrix[x + cell][y + 1 + cell]
+            self.move_group_arm.go(pose_position)
+
+    def draw_L(self, matrix, starting_index):
+        #top left corner
+        x, y = starting_index
+        for cell in range(0, 5):
+            pose_position = matrix[x][y + cell]
+            self.move_group_arm.go(pose_position)
+
+        for cell in range(0, 3):
+            pose_position = matrix[x + cell][y + 4]
+            self.move_group_arm.go(pose_position)
+
+    def draw_M(self, matrix, starting_index):
+        #bottom left corner
+        x, y = starting_index
+        for cell in range(0, 5):
+            pose_position = matrix[x][y - cell]
+            self.move_group_arm.go(pose_position)
+
+        for cell in range(0, 3):
+            pose_position = matrix[x + cell][y - 4 + cell]
+            self.move_group_arm.go(pose_position)
+
+        for cell in range(0, 3):
+            pose_position = matrix[x + 2 + cell][y - 2 - cell]
+            self.move_group_arm.go(pose_position)
+
+        for cell in range(0, 5):
+            pose_position = matrix[x + 4][y - 4 + cell]
+            self.move_group_arm.go(pose_position)
+            
+
+    def draw_N(self, matrix, starting_index):
+        #bottom left corner
+        x, y = starting_index
+        for cell in range(0, 5):
+            pose_position = matrix[x][y - cell]
+            self.move_group_arm.go(pose_position)
+
+        for cell in range(0, 4):
+            pose_position = matrix[x + cell][y - 4 + cell]
+            self.move_group_arm.go(pose_position)
+
+        #reset position
+
+        for cell in range(0, 5):
+            pose_position = matrix[x + 3][y - 4 + cell]
+            self.move_group_arm.go(pose_position)
+            
+
+    def draw_O(self, matrix, starting_index):
+
+    def draw_P(self, matrix, starting_index):
+
+    def draw_Q(self, matrix, starting_index):
+
+    def draw_R(self, matrix, starting_index):
+
+    def draw_S(self, matrix, starting_index):
+
+    def draw_T(self, matrix, starting_index):
+        #top left corner
+        x, y = starting_index
+        for cell in range(0, 3):
+            pose_position = matrix[x + cell][y]
+            self.move_group_arm.go(pose_position)
+
+        for cell in range(0, 5):
+            pose_position = matrix[x + 1][y + cell]
+            self.move_group_arm.go(pose_position)
+
+    def draw_U(self, matrix, starting_index):
+        #top left corner
+        x, y = starting_index
+        for cell in range(0, 5):
+            pose_position = matrix[x][y + cell]
+            self.move_group_arm.go(pose_position)
+
+        for cell in range(0, 4):
+            pose_position = matrix[x + cell][y + 4]
+            self.move_group_arm.go(pose_position)
+
+        #reset position
+        for cell in range(0, 5):
+            pose_position = matrix[x + 3][y + cell]
+            self.move_group_arm.go(pose_position)
+    
+
+    def draw_V(self, matrix, starting_index):
+
+    def draw_W(self, matrix, starting_index):
+
+    def draw_X(self, matrix, starting_index):
+
+    def draw_Y(self, matrix, starting_index):
+
+    def draw_Z(self, matrix, starting_index):
+
 
     def reset_arm(self):
         # left/right, whole arm up/down, forearm up/down, gripper angle
