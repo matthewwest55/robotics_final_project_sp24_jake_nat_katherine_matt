7b157fa733d51e72a93ffec14ac0226a4346d131
nugerhard <ngerhard@uchicago.edu>
2024-05-15 16:50:31 -0500

    Sequential guess processing added

--- scripts/demo_cam_local.py
+++ scripts/demo_cam_local.py
@@ -12,12 +12,15 @@ class GuessCam(object):
     def __init__(self):
         self.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
         self.guesses = []
+        # Tracks if we are looking for a guess
         self.new_image = True
+        # Part of control to ignore first guess
+        self.first = True
         self.cap = cv2.VideoCapture(0)
-        time.sleep(5)
+        time.sleep(.1) # Rospy TODO
         self.run()
 
-    def action_check(self, data):
+    def action_check(self, data): 
         self.new_image = True
     
     @staticmethod
@@ -26,15 +29,16 @@ class GuessCam(object):
         return x
     
     def run(self):
+
+        curr_guess = "Null"
+        curr_count = 0
+
         transform = v2.Compose([
             v2.Lambda(lambda x: v2.functional.invert(x)),
             v2.Grayscale(),
-            #v2.Lambda(lambda x: self.gs_threshold(x)),
-            #v2.Grayscale(),
             v2.Resize((28, 28)),
             v2.RandomRotation(degrees=(90, 90)),
             v2.RandomVerticalFlip(p=1.0),
-            #v2.Lambda(lambda x: v2.functional.invert(x)),
             v2.ToImage(),
             v2.ToDtype(torch.float32, scale=True),
             v2.Lambda(lambda x: self.gs_threshold(x)),
@@ -44,64 +48,71 @@ class GuessCam(object):
         mod = torch.load("letter_model2.pt")
         mod.eval()
 
-        while True:
+        while True: # This should eventually be rospy shutdown check TODO
 
-            ret, frame = self.cap.read()
-            if not ret:
-                print("Error capturing frame")
-                break
+            if self.new_image == True:
+                ret, frame = self.cap.read()
+                if not ret:
+                    print("Error capturing frame")
+                    break
 
-            # Assuming frame is the image captured from your camera
-            height, width, _ = frame.shape
+                # Assuming frame is the image captured from your camera
+                height, width, _ = frame.shape
 
-            # Calculate the size of the square region to crop
-            size = min(height, width)
+                # Calculate the size of the square region to crop
+                size = min(height, width)
 
-            # Calculate the coordinates for cropping
-            top = int(height * 0.4)
-            bottom = int(height * .6
-                         )
-            left = int((width / 2)-(height*.1))
-            right = int((width/ 2) + (height*.1))
-            cropped = frame[top:bottom, left:right]
+                # Calculate the coordinates for cropping
+                top = int(height * 0.4)
+                bottom = int(height * .6
+                            )
+                left = int((width / 2)-(height*.1))
+                right = int((width/ 2) + (height*.1))
+                cropped = frame[top:bottom, left:right]
 
-            # Display the camera feed
-            cv2.imshow("Camera Feed", frame)
-            cv2.waitKey(1)
-            """
-            # Convert the frame to PIL image
-            # Convert the image to HSV format
-            hsv_image = cv2.cvtColor(cropped, cv2.COLOR_BGR2RGB)
-            plt.imshow(hsv_image)
-            plt.savefig('image6-threshold.png')
-            plt.show()
-            """
-            # Split the HSV image into its components
-            hsv_image = cv2.cvtColor(cropped, cv2.COLOR_BGR2HSV)
-            """h, s, v = cv2.split(hsv_image)
-            v *= 2 
-            for i in range(len(v)):
-                for j in range(len(v[0])):
-                    v[i][j] = min(255, v[i][j])
-            hsv_image = cv2.merge([h, s, v])"""
+                # Display the camera feed
+                cv2.imshow("Camera Feed", frame)
+                cv2.waitKey(.5)
 
-            rgb_image = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2RGB)
-            pil_image = Image.fromarray(rgb_image)
-            transformed_image = transform(pil_image)
-            plt.imshow(torch.squeeze(transformed_image), cmap='gray')
-            plt.savefig('image6-ringlight.png')
-            plt.show()
+                rgb_image = cv2.cvtColor(cropped, cv2.COLOR_BGR2RGB)
+                pil_image = Image.fromarray(rgb_image)
+                transformed_image = transform(pil_image)
+                # Visualize processed image (for debugging)
+                """plt.imshow(torch.squeeze(transformed_image), cmap='gray')
+                plt.savefig('image6-ringlight.png')
+                plt.show()"""
 
-            # Predict the letter
-            with torch.no_grad():
-                output = mod(transformed_image)
-            probs = torch.softmax(output, dim=1)
-            predicted_class = torch.argmax(probs, dim=1).item()
-            print("This is the class: ", predicted_class)
-            letter = self.alphabet[predicted_class]
+                # Predict the letter
+                with torch.no_grad():
+                    output = mod(transformed_image)
+                probs = torch.softmax(output, dim=1)
+                predicted_class = torch.argmax(probs, dim=1).item()
 
-            print("This is the letter: ", letter)
-            time.sleep(1)
+
+                letter = self.alphabet[predicted_class]
+                
+
+                if letter == curr_guess:
+                    curr_count += 1
+                else:
+                    self.first = False
+                    curr_count = 1
+                    curr_guess = letter
+                print(f"[{curr_count}/4] Prediction: {letter}.")
+
+                if (curr_count == 5) and not (curr_guess in self.guesses):
+                    if self.first:
+                        curr_count = 1
+                    else:
+                        # send off msg
+                        self.guesses.append(letter)
+                        self.new_image = False
+                        print(f"[!!!] Letter Guessed: {letter}.")
+
+            time.sleep(.5) # Rospy TODO
+
+
+
             
 if __name__ == '__main__':
     demo = GuessCam()
